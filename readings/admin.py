57616dd40c4de25e62d01ea6e6d8c393c102c4a7
Jared Kerim <jared@cumulonimbus.ca>
2013-10-05 21:58:23 +0000

    Removed readings graphs from reading admin page

--- readings/admin.py
+++ readings/admin.py
@@ -15,44 +15,6 @@ class ReadingAdmin(admin.ModelAdmin):
     def has_add_permission(self, *args, **kwargs):
         return False
 
-    def changelist_view(self, request, extra_context=None):
-        now = datetime.datetime.now()
-        current_date = datetime.datetime(now.year, now.month, now.day, now.hour)
-
-        readings_per_hour = []
-        active_users_per_hour = []
-        for num_hours in range(1, 150):
-            start_date = to_unix(current_date - datetime.timedelta(hours=num_hours))
-            end_date = to_unix(current_date - datetime.timedelta(hours=(num_hours - 1)))
-
-            cache_key = 'admin:active_users:%s:%s' % (start_date, end_date)
-            readings = cache.get(cache_key)
-
-            if not readings:
-                readings = Reading.objects.all().filter(daterecorded__gte=start_date, daterecorded__lte=end_date).count()
-                cache.set(cache_key, readings, 9999999999)
-
-            readings_per_hour.append([end_date, readings])
-
-            cache_key = 'admin:readings_per_hour:%s:%s' % (start_date, end_date)
-            active_users = cache.get(cache_key)
-
-            if not active_users:
-                active_users = Reading.objects.all().filter(daterecorded__gte=start_date, daterecorded__lte=end_date).values_list('user_id').distinct().count()
-                cache.set(cache_key, active_users, 9999999999)
-
-            active_users_per_hour.append([end_date, active_users])
-
-        context = {
-            'readings_data': json.dumps(readings_per_hour),
-            'active_user_data': json.dumps(active_users_per_hour),
-        }
-
-        if extra_context:
-            context.update(extra_context)
-
-        return super(ReadingAdmin, self).changelist_view(request, context)
-
 admin.site.register(Reading, ReadingAdmin)
 
 
