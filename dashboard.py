f135962bf183143ca7e02d878e41e38ee9008ad5
Jared Kerim <jared@cumulonimbus.ca>
2013-10-30 04:13:27 +0000

    Removed unused imports, whitespace

--- dashboard.py
+++ dashboard.py
@@ -8,12 +8,10 @@ To activate your index dashboard add the following to your settings.py::
 import random
 
 from django.utils.translation import ugettext_lazy as _
-from django.core.urlresolvers import reverse
 from django.core.cache import cache
 
 
 from grappelli.dashboard import modules, Dashboard
-from grappelli.dashboard.utils import get_admin_site_name
 
 
 class CacheModule(modules.DashboardModule):
@@ -39,31 +37,8 @@ class PressureNETIndexDashboard(Dashboard):
     """
     Custom index dashboard for www.
     """
-    
+
     def init_with_context(self, context):
-        site_name = get_admin_site_name(context)
-        
-        # append a group for "Administration" & "Applications"
-        #self.children.append(modules.Group(
-        #    _('Group: Administration & Applications'),
-        #    column=1,
-        #    collapsible=True,
-        #    children = [
-        #        modules.AppList(
-        #            _('Administration'),
-        #            column=1,
-        #            collapsible=False,
-        #            models=('django.contrib.*',),
-        #        ),
-        #        modules.AppList(
-        #            _('Applications'),
-        #            column=1,
-        #            css_classes=('collapse closed',),
-        #            exclude=('django.contrib.*',),
-        #        )
-        #    ]
-        #))
-        
         # append an app list module for "Administration"
         self.children.append(modules.ModelList(
             _('Administration'),
@@ -71,7 +46,7 @@ class PressureNETIndexDashboard(Dashboard):
             collapsible=True,
             models=('django.contrib.*',),
         ))
-        
+
         # append an app list module for "Applications"
         self.children.append(modules.ModelList(
             _('Readings'),
@@ -79,7 +54,7 @@ class PressureNETIndexDashboard(Dashboard):
             column=1,
             models=('readings.*',),
         ))
-        
+
         # append an app list module for "Applications"
         self.children.append(modules.ModelList(
             _('Customers'),
@@ -88,57 +63,12 @@ class PressureNETIndexDashboard(Dashboard):
             models=('customers.*',),
         ))
 
-
         # Cache module
         self.children.append(CacheModule(
             _('Cache Status'),
             column=2,
         ))
 
-        ## append another link list module for "support".
-        #self.children.append(modules.LinkList(
-        #    _('Media Management'),
-        #    column=2,
-        #    children=[
-        #        {
-        #            'title': _('FileBrowser'),
-        #            'url': '/admin/filebrowser/browse/',
-        #            'external': False,
-        #        },
-        #    ]
-        #))
-        #
-        ## append another link list module for "support".
-        #self.children.append(modules.LinkList(
-        #    _('Support'),
-        #    column=2,
-        #    children=[
-        #        {
-        #            'title': _('Django Documentation'),
-        #            'url': 'http://docs.djangoproject.com/',
-        #            'external': True,
-        #        },
-        #        {
-        #            'title': _('Grappelli Documentation'),
-        #            'url': 'http://packages.python.org/django-grappelli/',
-        #            'external': True,
-        #        },
-        #        {
-        #            'title': _('Grappelli Google-Code'),
-        #            'url': 'http://code.google.com/p/django-grappelli/',
-        #            'external': True,
-        #        },
-        #    ]
-        #))
-        #
-        ## append a feed module
-        #self.children.append(modules.Feed(
-        #    _('Latest Django News'),
-        #    column=2,
-        #    feed_url='http://www.djangoproject.com/rss/weblog/',
-        #    limit=5
-        #))
-        #
         # append a recent actions module
         self.children.append(modules.RecentActions(
             _('Recent Actions'),
@@ -146,5 +76,3 @@ class PressureNETIndexDashboard(Dashboard):
             collapsible=False,
             column=3,
         ))
-
-
