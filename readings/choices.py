9c3a32cf204131616c872c55b69d9db04d0f0e6b
Jared Kerim <jared@cumulonimbus.ca>
2014-01-02 06:06:07 +0000

    Added Nobody sharing option #56

--- readings/choices.py
+++ readings/choices.py
@@ -2,10 +2,12 @@ SHARING_PUBLIC = 'Public'
 SHARING_RESEARCHERS_FORECASTERS = 'Us, Researchers and Forecasters'
 SHARING_RESEARCHERS = 'Us and Researchers'
 SHARING_PRIVATE = 'Cumulonimbus (Us)'
+SHARING_NOBODY = 'Nobody'
 
 SHARING_CHOICES = (
     (SHARING_PUBLIC, 'Public'),
     (SHARING_RESEARCHERS_FORECASTERS, 'Researchers and Forecasters'),
     (SHARING_RESEARCHERS, 'Researchers'),
     (SHARING_PRIVATE, 'Private'),
+    (SHARING_NOBODY, 'Nobody'),
 )
