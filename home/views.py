e1f64e4959e7f59c7ab15e8b32b3602cd71fcc1d
Jared Kerim <jared@cumulonimbus.ca>
2014-01-15 23:31:16 +0000

    Added map page, fixed mobile sizing

--- home/views.py
+++ home/views.py
@@ -5,3 +5,4 @@ from django.views.generic.base import TemplateView
 
 index = cache_page(TemplateView.as_view(template_name='home/index.html'), settings.CACHE_TIMEOUT)
 about = cache_page(TemplateView.as_view(template_name='home/about.html'), settings.CACHE_TIMEOUT)
+map_page = cache_page(TemplateView.as_view(template_name='home/map.html'), settings.CACHE_TIMEOUT)
