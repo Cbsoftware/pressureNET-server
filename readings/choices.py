31b45088f7742856c0c8862c41589e0daafff104
Jared Kerim <jared@cumulonimbus.ca>
2014-04-26 19:05:52 +0000

    Rejected nobody data #96

--- readings/choices.py
+++ readings/choices.py
@@ -2,12 +2,13 @@ SHARING_PUBLIC = 'Public'
 SHARING_RESEARCHERS_FORECASTERS = 'Us, Researchers and Forecasters'
 SHARING_RESEARCHERS = 'Us and Researchers'
 SHARING_PRIVATE = 'Cumulonimbus (Us)'
-SHARING_NOBODY = 'Nobody'
+#SHARING_NOBODY = 'Nobody'
 
 SHARING_CHOICES = (
     (SHARING_PUBLIC, 'Public'),
     (SHARING_RESEARCHERS_FORECASTERS, 'Researchers and Forecasters'),
     (SHARING_RESEARCHERS, 'Researchers'),
     (SHARING_PRIVATE, 'Private'),
-    (SHARING_NOBODY, 'Nobody'),
+    # 'Nobody' data should be rejected #96
+    #(SHARING_NOBODY, 'Nobody'),
 )
