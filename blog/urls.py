4a450dc374484000ffc2646924ec5b4801365e61
Jared Kerim <jared@cumulonimbus.ca>
2014-05-17 02:57:26 +0000

    Updated blog pages

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'blog.views',
     url('^$', 'blog_list', name='blog-list'),
+    url('^author/(?P<author>[-_\w]+)/$', 'blog_list_author', name='blog-list-author'),
     url(r'^(?P<slug>[-_\w]+)/$', 'blog_detail', name='blog-detail'), 
 )
