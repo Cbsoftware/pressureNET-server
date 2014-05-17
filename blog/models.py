ca3b30e05b2a71cffb5685236c146985c17b2b7b
Jared Kerim <jared@cumulonimbus.ca>
2014-05-17 21:49:43 +0000

    Added auto blur image field to blog post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from imagekit.models import ImageSpecField
+
+from utils.image import GaussianBlurSpec
+
 
 class BlogPost(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -9,7 +13,7 @@ class BlogPost(models.Model):
     slug = models.SlugField(max_length=255, unique=True)
     teaser = models.TextField()
     image = models.ImageField(max_length=255, upload_to='blog/images', blank=True, null=True)
-    image_blurred = models.ImageField(max_length=255, upload_to='blog/images', blank=True, null=True)
+    image_blurred = ImageSpecField(source='image', processors=[GaussianBlurSpec()], format='JPEG')
     content = models.TextField()
     published = models.BooleanField()
 
